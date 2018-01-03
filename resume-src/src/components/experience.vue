95b619e6b0b715fe941c5eea0e7440669c69a5ff
Steven Brien <sbrien@hlkagency.com>
2018-01-03 14:29:04 -0600

    Framework and style updates

---
+++ resume-src/src/components/experience.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="experience row">
+    <div class="columns small-12">
+      <h3>Work Experience</h3>
+      <job v-for="item in data" :data="item" :key="item.company"></job>
+    </div>
+  </div>
+</template>
+
+<script>
+import job from '@/components/job'
+
+export default {
+  name: 'experience',
+  props: ['data'],
+  components: {
+    job
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ resume-src/src/components/skills.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="skills row">
+    <div class="columns small-12">
+      <h3>Skills</h3>
+      <div class="row">
+        <div class="columns padding-sm small-12 medium-6 " v-for="item in data">
+          <h5>
+            {{item.name}}
+          </h5>
+          <ul>
+            <li v-for="skill in item.keywords">{{skill}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'skills',
+  props: ['data'],
+}
+</script>
+
+<style>
+</style>
